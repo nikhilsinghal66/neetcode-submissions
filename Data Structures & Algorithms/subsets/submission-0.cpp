6995d369266c3fd5864a944a16cb584e6a51ade6
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-02 23:54:54 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void solve(vector<int> &nums,vector<int> &temp,vector<vector<int>> &ans,int ind){
+        if(ind==nums.size()){
+            ans.push_back(temp);
+            return ;
+        }
+        temp.push_back(nums[ind]);
+        solve(nums,temp,ans,ind+1);
+        temp.pop_back();
+        solve(nums,temp,ans,ind+1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(nums,temp,ans,0);
+        return ans;
+    }
+};
