4e6140e348e0ef3633978203c0a84e129d369474
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-05 13:39:32 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*,Node*> mpp;
+    Node* cloneGraph(Node* node) {
+        if(node==NULL){
+            return NULL;
+        }
+        if(mpp.find(node)!=mpp.end()){
+            return mpp[node];
+        }
+        Node* clone=new Node(node->val);
+        mpp[node]=clone;
+        for(Node* newNode:node->neighbors){
+            clone->neighbors.push_back(cloneGraph(newNode));
+        }
+        return clone;
+    }
+};
