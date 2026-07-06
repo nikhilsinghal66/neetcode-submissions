01737c9d59144fe45df8fec4d74f67765728ef64
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-06 17:25:37 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // int max(int a,int b){
+    //     if(a>b){
+    //         return a;
+    //     }
+    //     return b;
+    // }
+
+    int maxAmount(int ind,vector<int> &dp,vector<int> &nums){
+        if(ind==0){
+            return nums[ind];
+        }
+        if(ind<0){
+            return 0;
+        }
+        if(dp[ind]!=-1) return dp[ind];
+        //Skip it...
+        int skip_step=0 + maxAmount(ind-1,dp,nums);
+        //Take it...
+        int right=0;
+        if(ind>=1){
+            right=nums[ind]+maxAmount(ind-2,dp,nums);
+        }
+        return dp[ind]=max(skip_step,right);
+    }
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return maxAmount(n-1,dp,nums);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
